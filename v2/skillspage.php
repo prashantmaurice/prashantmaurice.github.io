ee27df6ddf09d1d850d721d8df0ef2ebebb2fb6e
Maurice <sabertoothmaurice@gmail.com>
2014-11-09 06:08:01 +0530

    skills page diamonds new version

--- v2/skillspage.php
+++ v2/skillspage.php
@@ -3,28 +3,28 @@
         <!-- Page Heading -->
         <div class="row" style="margin: 0px; text-align: right; vertical-align: middle; height:100%; color:#fff">
             <div style="position: relative; vertical-align: middle; top:10%">
-                <div class="diamond" style="top:-103px;left:0px;">
+                <div class="diamond" style="top:0px;left:0px;">
                     <div class="diamond-content"><div class="diamond-content2">HTML</div></div>
                 </div>
-                <div class="diamond" style="top:0px;left:103px">
+                <div class="diamond" style="top:103px;left:103px">
                     <div class="diamond-content"><div class="diamond-content2">JAVASCRIPT</div></div>
                 </div>
-                <div class="diamond" style="top:-103px;left:206px">
+                <div class="diamond" style="top:0px;left:206px">
                     <div class="diamond-content"><div class="diamond-content2">CSS</div></div>
                 </div>
-                <div class="diamond" style="top:0px;left:309px">
+                <div class="diamond" style="top:103px;left:309px">
                     <div class="diamond-content"><div class="diamond-content2">NODE.JS</div></div>
                 </div>
-                <div class="diamond" style="top:-103px;left:412px">
+                <div class="diamond" style="top:0px;left:412px">
                     <div class="diamond-content"><div class="diamond-content2">ANDROID</div></div>
                 </div>
-                <div class="diamond" style="top:0px;left:515px">
+                <div class="diamond" style="top:103px;left:515px">
                     <div class="diamond-content"><div class="diamond-content2">PHP</div></div>
                 </div>
-                <div class="diamond" style="top:103px;left:412px">
+                <div class="diamond" style="top:206px;left:412px">
                     <div class="diamond-content"><div class="diamond-content2">MYSQL</div></div>
                 </div>
-                <div class="diamond" style="top:206px;left:309px">
+                <div class="diamond" style="top:309px;left:309px">
                     <div class="diamond-content"><div class="diamond-content2">PHONEGAP</div></div>
                 </div>
             </div>
