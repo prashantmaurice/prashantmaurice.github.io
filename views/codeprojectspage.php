26d17ce9dcfb1369d8f2c5421d15abad6a40d82c
maurice <sabertoothmaurice@gmail.com>
2015-01-21 13:19:15 +0530

    fixed code projects default project

--- views/codeprojectspage.php
+++ views/codeprojectspage.php
@@ -5,14 +5,18 @@
     $number = count($projects)-1;
 
     $projectcode = (isset($_GET["project"])? $_GET["project"] : "");
+    $projectId = 3;//default
     for ($x=1; $x<=$number; $x++) {
         $link = "";
         if ($projects[$x]['code'] == $projectcode) {
             $link = $projects[$x]['link'];
+            $projectId = $x;
             break;
         }
     }
+echo $link;
 ?>
+
 <div id="androidprojects-page">
     <div class="container-fluid" style="height:100%; padding:0px">
         <div class="row" style="height:100%; margin:0px">
@@ -20,7 +24,8 @@
                 <ul id="ProjectsList">
                     <?php
                         for ($x=1; $x<=$number; $x++) {
-                            echo '<li class="projectslist_li  codeProject" data-link="'.$projects[$x]['link'].'">';
+                            echo $projects[$x]['link'];
+                            echo '<li class="projectslist_li '.(($x==$projectId)?"active":"").' codeProject" data-link="'.$projects[$x]['link'].'">';
                             echo '<h4>'.$projects[$x]['projectName'].'</h4><h6>'.$projects[$x]['time'].'</h6><br><h5>'.$projects[$x]['description'].'<h5>';
                             echo '</li>';
                         }
@@ -28,7 +33,7 @@
                 </ul>
             </div>
             <div class="col-lg-9" style=" padding:10px" id="codeRightCont">
-                <div id="iframeWrapper"><iframe id="scaled-frame" src="http://wethechangemission.org/"></iframe></div>
+                <div id="iframeWrapper"><iframe id="scaled-frame" src="<?php echo $projects[$projectId]['link'];?>"></iframe></div>
             </div>
         </div>
         <ul id="Grid" style="padding:0">
